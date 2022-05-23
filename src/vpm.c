3741cf54ddff0af8fd3397ad37f6dfc92f576b45
farkon00 <sammer2016sammer@gmail.com>
2022-05-23 22:17:45 +0300

    -v -e moved to there own functions

--- src/vpm.c
+++ src/vpm.c
@@ -17,6 +17,7 @@ char *shift(int *argc, char ***argv)
 void usage(char *program_name)
 {
   fprintf(stderr, "[USAGE] %s <flags> [arguments]\n", program_name);
+  fprintf(stderr, "-r <input.vpasm> [-o output.vpm]   Compile vpasm file to a .vpm binary file and execute output file.\n");
   fprintf(stderr, "-v <input.vpasm> [-o output.vpm]   Compile vpasm file to a .vpm binary file.\n");
   fprintf(stderr, "-e <program.vpm>                   Execute a .vpm file\n");
   fprintf(stderr, "-h --help                          Display this help message.\n");
@@ -60,6 +61,72 @@ void program_from_bin_file(Program* program, char* file_path)
   fclose(f);
 }
 
+int compile_vpasm_to_bin(int argc, char **argv, char *program_name)
+{
+  if (argc < 1) {
+    usage(program_name);
+    exit(1);
+  }
+
+  char* input_file = shift(&argc, &argv);
+  int shifted = 1;
+  char* output_file = "output.vpm";
+  if (argc == 0) {
+    // No output path provided
+    String_View extension = cstr_to_sv(input_file);
+    String_View input_name = sv_chop_by_delimiter(&extension, '.');
+
+    output_file = strcat(sv_to_cstr(input_name), ".vpm");
+  } else {
+    char *flag = shift(&argc, &argv);
+    shifted++;
+    if (strcmp(flag, "-o") != 0) {
+      usage(program_name);
+      fprintf(stderr, "Unknown flag: %s\n", flag);
+      exit(1);
+    }
+    if (argc == 0) {
+      usage(program_name);
+      fprintf(stderr, "[ERROR] No output file name provided for flag -o\n");
+      exit(1);
+    }
+    output_file = shift(&argc, &argv);
+    shifted++;
+  }
+
+  Program program = {0};
+
+  vpasm_load_from_file(&program, input_file);
+
+  program_as_bin_file(&program, output_file);
+
+  return shifted;
+}
+
+void execute_bin_file(int argc, char **argv, char *program_name)
+{
+  if (argc < 1) {
+    usage(program_name);
+    exit(1);
+  }
+
+  char* input_file = shift(&argc, &argv);
+  printf("%s\n", input_file);
+  Memory memory = {0};
+  Program program = {0};
+
+  vpasm_initialize_registers(&memory);
+
+  program_from_bin_file(&program, input_file);
+
+  vpasm_load_program(&memory, &program);
+  vpasm_exec_program(&memory, 100, false);
+
+  vpasm_debug_print_registers(stdout, &memory);
+  vpasm_free(&memory);
+  exit(0);
+}
+
 int main(int argc, char **argv)
 {
   char* program_name = shift(&argc, &argv);
@@ -77,61 +144,12 @@ int main(int argc, char **argv)
       usage(program_name);
       exit(0);
     } else if (strcmp(flag, "-v") == 0) {
-      if (argc < 1) {
-        usage(program_name);
-        exit(1);
+      int shifted = compile_vpasm_to_bin(argc, argv, program_name);
+      for (int i = 0; i < shifted; i++) {
+        shift(&argc, &argv);
       }
-
-      char* input_file = shift(&argc, &argv);
-      char* output_file = "output.vpm";
-      if (argc == 0) {
-        // No output path provided
-        String_View extension = cstr_to_sv(input_file);
-        String_View input_name = sv_chop_by_delimiter(&extension, '.');
-
-        output_file = strcat(sv_to_cstr(input_name), ".vpm");
-      } else {
-        char *flag = shift(&argc, &argv);
-        if (strcmp(flag, "-o") != 0) {
-          usage(program_name);
-          fprintf(stderr, "Unknown flag: %s\n", flag);
-          exit(1);
-        }
-        if (argc == 0) {
-          usage(program_name);
-          fprintf(stderr, "[ERROR] No output file name provided for flag -o\n");
-          exit(1);
-        }
-        output_file = shift(&argc, &argv);
-      }
-
-      Program program = {0};
-
-      vpasm_load_from_file(&program, input_file);
-
-      program_as_bin_file(&program, output_file);
-
     } else if (strcmp(flag, "-e") == 0) {
-      if (argc < 1) {
-        usage(program_name);
-        exit(1);
-      }
-
-      char* input_file = shift(&argc, &argv);
-      printf("%s\n", input_file);
-      Memory memory = {0};
-      Program program = {0};
-
-      vpasm_initialize_registers(&memory);
-
-      program_from_bin_file(&program, input_file);
-
-      vpasm_load_program(&memory, &program);
-      vpasm_exec_program(&memory, 100, false);
-
-      vpasm_debug_print_registers(stdout, &memory);
-      vpasm_free(&memory);
-      exit(0);
+      execute_bin_file(argc, argv, program_name);
     } else {
       usage(program_name);
       fprintf(stderr, "[ERROR] Unknown flag %s\n", flag);
